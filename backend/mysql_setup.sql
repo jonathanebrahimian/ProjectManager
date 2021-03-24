b465e22551b1ea9b9557d3406ff452a16a42c72f
JonyZizou <jonyzizou@gmail.com>
2021-03-23 17:30:36 -0700

    intialization scripts

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -4,13 +4,44 @@ CREATE DATABASE db;
 -- use newly create database
 USE db;
 
--- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+-- create Materials Table
+CREATE TABLE `db`.`Materials` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45),
+    `status` INT,
+    `quality` INT,
+    FOREIGN KEY(`project`) REFERENCES `Project`.`id`,
+    `supplierUser` INT,
+    PRIMARY KEY(`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+    
+
 );
+
+-- Users Lauren
+create table users (
+    userID int
+	AUTO_INCREMENT primary key
+    , userType int
+    , userFirst varchar(50)
+    , userLast varchar (50)
+    , project int
+    , foreign key (project) references goals (goalID)
+    , reportsTo int default null
+    , foreign key (reportsTo) references users (userID)
+);
+
+
+-- create Goals Table
+CREATE TABLE goals(
+    goalID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (goalID) REFERENCES 
+    goalName varchar(20),
+    goalNotes varchar(100),
+    materials varchar(30),
+    constructionID INT
+);
+
 
 -- insert sample entry
 INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
