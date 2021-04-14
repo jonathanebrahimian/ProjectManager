42955b4cbb1288ca7f8d77c8852adfbb6378aebf
Lauren H <lhorton@smu.edu>
2021-04-13 20:04:11 -0500

    edited APIs

--- backend/routes.js
+++ backend/routes.js
@@ -216,31 +216,5 @@ module.exports = function routes(app, logger) {
       });
     });
 
-    // GET /sites/{userID}    sites user is a part of
-    app.get('/materials/:siteID', (req, res) => {
-      //console.log(req.body.product);
-      // obtain a connection from our pool of connections
-      pool.getConnection(function (err, connection){
-        if(err){
-          // if there is an issue obtaining a connection, release the connection instance and log the error
-          logger.error('Problem obtaining MySQL connection',err)
-          res.status(400).send('Problem obtaining MySQL connection'); 
-        } else {
-        let userID = req.params.userID;
-  
-        //console.log(req.param);
-          // if there is no issue obtaining a connection, execute query and release connection
-          connection.query("SELECT s.* FROM sites s INNER JOIN roster r ON r.siteID = s.siteID WHERE r.userID = ?", [userID], function (err, rows, fields) {
-            connection.release();
-            if (err) {
-              // if there is an error with the query, log the error
-              logger.error("Problem getting from test table: \n", err);
-              res.status(400).send('Problem getting from table'); 
-            } else {
-              res.end(JSON.stringify(result));
-            }
-          });
-        }
-      });
-    });
+    
 }
