f895acb7adcc7a4035fe367d8bbe007918028130
jonathanebrahimian <jebrahimian@smu.edu>
2021-04-22 22:42:01 -0500

    adding announcments

--- backend/routes.js
+++ backend/routes.js
@@ -1028,7 +1028,39 @@ module.exports = function routes(app, logger) {
         }
       });
     });
-
+  
+  // POST /equipment{siteID}
+  app.post('/announcements', (req, res) => {
+    //console.log(req.body.product);
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection'); 
+      } else {
+      let equipmentID = req.body.equipmentID;
+      let equipmentName = req.body.equipmentName;
+      let location = req.body.location;
+      let siteID = req.body.siteID;
+      let userID = req.body.userID;
+      
+      //console.log(req.param);
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query("INSERT INTO equipment (equipmentID, equipmentName, location, siteID, userID) VALUES (?, ?, ?, ?, ?)", [equipmentID, equipmentName, location, siteID, userID], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            // if there is an error with the query, log the error
+            logger.error("Problem inserting into test table: \n", err);
+            res.status(400).send('Problem inserting into table'); 
+          } else {
+            //res.end(JSON.stringify(result));
+            res.status(200).send(`added to the table!`);
+          }
+        });
+      }
+    });
+  });
 
     // POST /register
   app.post('/register', (req, res) => {
